e49a4e5587f19e2ceb3d91e0e8f01bf87cc60d59
Archdoog <jf0517@gmail.com>
2018-07-20 09:23:22 -0600

    Untested CoreData method implemented

--- PrivacyKit/Classes/Models/PrivacyModel.swift
+++ PrivacyKit/Classes/Models/PrivacyModel.swift
@@ -10,13 +10,8 @@ import CoreData
 
 class PrivacyModel {
     
-    /** Core Data Specifications */
-    lazy var managedObjectModel: NSManagedObjectModel = {
-        let privacyKitBundle = Bundle(identifier: "com.ithazfire.PrivacyKit")
-        
-        let modelURL = privacyKitBundle!.url(forResource: "Privacy", withExtension: "momd")!
-        return NSManagedObjectModel(contentsOf: modelURL)!
-    }()
+    /** Core Data Stack */
+    fileprivate let context = CoreDataStack.managedObjectContext
     fileprivate var privacy: Privacy?
     
     /** Data Keys */
@@ -27,14 +22,51 @@ class PrivacyModel {
     }
     
     func buildDefaults() {
+        guard let entity = NSEntityDescription.entity(forEntityName: "Privacy", in: context) else {
+            print("PrivacyModel: Error creating build entity")
+            return
+        }
         
+        if let current = self.fetch() {
+            /** Already Built */
+            return
+        }
+        
+        privacy = NSManagedObject(entity: entity, insertInto: context) as! Privacy
+        privacy?.setValue(privacyAccepted, forKey: "privacyAccepted")
+        do {
+            try context.save()
+        } catch {
+            print("PrivacyModel: Error saving initial build context")
+        }
     }
     
-    func fetch() {
+    func fetch() -> Privacy? {
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Privacy")
+        request.returnsObjectsAsFaults = false
         
+        do {
+            let result = try context.fetch(request)
+            
+            if let row = result.first {
+                let privacy = row as! Privacy
+                privacyAccepted = privacy.value(forKey: "privacyAccepted") as! Bool
+                return privacy
+            }
+        } catch {
+            print("PrivacyModel: Failed to request context")
+        }
+        
+        return nil
     }
     
     func save() {
         privacy?.setValue(privacyAccepted, forKey: "privacyAccepted")
+        
+        do {
+            try context.save()
+        } catch {
+            print("PrivacyModel: Error saving context")
+        }
     }
 }
